509f9625874d4e9849dfdbd6cd37db1540637546
arthurkroth <kroth.sc@gmail.com>
2025-11-20 16:57:02 +0000

    Bug fix on the flash banner, also fixed the bug preventing to display information about the last and current login on the Account tab.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_08_201318) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_20_164643) do
   create_table "projects", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name", null: false
@@ -46,6 +46,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_08_201318) do
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
     t.integer "manager_id"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["manager_id"], name: "index_users_on_manager_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
